1e0f147f0213845c52dfb9acaf4163261c23c401
Benjamin van Stein <stein.van2@stud.uni-corvinus.hu>
2021-04-22 11:25:55 +0200

    9.gyak

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace HajosTeszt.Controllers
 {
-    [ApiController]
     public class BoatController : Controller
     {
         [HttpGet]
@@ -18,6 +17,20 @@ namespace HajosTeszt.Controllers
             var kérdések = from x in context.Questions select x.QuestionText;
 
             return new JsonResult(kérdések);
+        }
+
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
     }
 }
--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,6 +1,6 @@
-﻿var kerdesek = [];
-var akt_kérdés = 0;
-function letöltés() {
+﻿var kerdes;
+var akt_kérdés = 1;
+/*function letöltés() {
     fetch('/question.json')
         .then(r => r.json())
         .then(d => letöltésbefejeződött(d));
@@ -24,24 +24,73 @@ function kérdésMegjelenítés(kérdés) {
     valasz2.innerHTML = kerdesek[kérdés].answer2;
     valasz3.innerHTML = kerdesek[kérdés].answer3;
     kep.setAttribute("src", "https://szoft1.comeback.hu/hajo/" + kerdesek[0].image)
-    }
+}*/
+
+/*fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );*/
+
+function kérdésMegjelenítés(kérdés) {
+    document.getElementById("kep").onerror = "";
+    console.log(kérdés);
+    document.getElementById("kerdes").innerText = kérdés.questionText;
+    document.getElementById("valasz1").innerText = kérdés.answer1;
+    document.getElementById("valasz2").innerText = kérdés.answer2;
+    document.getElementById("valasz3").innerText = kérdés.answer3;
+    document.getElementById("kep").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+    if (kérdés.image == "") document.getElementById("kep").onerror = "this.style.display='none'";
+    kerdes = kérdés;
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}  
+
 function előre() {
-    if (akt_kérdés + 1 < kerdesek.length) {
+    fetch(`/questions/${akt_kérdés + 1}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                akt_kérdés++;
+            }
+        })
+    /*if (akt_kérdés + 1 < kerdesek.length) {
         akt_kérdés = akt_kérdés + 1;
     } else {
-        akt_kérdés = 0;
-    }
-    kérdésMegjelenítés(akt_kérdés);
+        akt_kérdés = 1;
+    }*/
+    kérdésBetöltés(akt_kérdés);
     töröl();
 }
 function vissza() {
-    if (akt_kérdés - 1 < 0) {
+    fetch(`/questions/${akt_kérdés - 1}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                akt_kérdés--;
+            }
+        })
+    /*if (akt_kérdés - 1 < 0) {
         akt_kérdés = kerdesek.length - 1;
     } else {
 
         akt_kérdés = akt_kérdés - 1;
-    }
-    kérdésMegjelenítés(akt_kérdés);
+    }*/
+    kérdésBetöltés(akt_kérdés);
     töröl();
 }
 function valasz() {
@@ -51,17 +100,17 @@ function valasz() {
     v1.classList.remove("kattinthato");
     v2.classList.remove("kattinthato");
     v3.classList.remove("kattinthato");
-    if (kerdesek[akt_kérdés].correctAnswer == 1) {
+    if (kerdes.correctAnswer == 1) {
         v1.classList.add("helyes");
         v2.classList.add("helytelen");
         v3.classList.add("helytelen");
     }
-    if (kerdesek[akt_kérdés].correctAnswer == 2) {
+    if (kerdes.correctAnswer == 2) {
         v2.classList.add("helyes");
         v1.classList.add("helytelen");
         v3.classList.add("helytelen");
     }
-    if (kerdesek[akt_kérdés].correctAnswer == 3) {
+    if (kerdes.correctAnswer == 3) {
         v3.classList.add("helyes");
         v2.classList.add("helytelen");
         v1.classList.add("helytelen");
@@ -83,4 +132,5 @@ function töröl() {
     v3.classList.remove("helytelen");
 }
 
-window.onload = letöltés();
+/*window.onload = letöltés();*/
+window.onload = kérdésBetöltés(akt_kérdés);
